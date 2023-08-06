2e73db4cfc0aa65435e2d89077799eb1488c4756
JaseGrable <jase.grable@gmail.com>
2023-08-06 15:45:52 -0700

    Debugging, wip

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -14,14 +14,17 @@ namespace Factory
 
             builder.Services.AddControllersWithViews();
 
-            builder.Services.AddDbContext<FactoryContext>
-                 (dbContextOptions => dbContextOptions.UseMySql
-                 (builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
-                 )
-                 );
+            builder.Services.AddDbContext<FactoryContext>(
+                              dbContextOptions => dbContextOptions
+                                .UseMySql(
+                                  builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                                )
+                              )
+                            );
 
             WebApplication app = builder.Build();
 
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -29,8 +32,7 @@ namespace Factory
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}"
-              );
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
