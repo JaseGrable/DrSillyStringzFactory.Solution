69d24fce6d4c9749989bee6f1ba24364147a8ac1
JaseGrable <jase.grable@gmail.com>
2023-08-10 15:58:27 -0700

    Add user model to project, update databse, update project files

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -2,39 +2,43 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Factory.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Factory
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
 
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllersWithViews();
+      builder.Services.AddControllersWithViews();
 
-            builder.Services.AddDbContext<FactoryContext>(
-                              dbContextOptions => dbContextOptions
-                                .UseMySql(
-                                  builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                                )
-                              )
-                            );
+      builder.Services.AddDbContext<FactoryContext>(
+                        dbContextOptions => dbContextOptions
+                          .UseMySql(
+                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                          )
+                        )
+                      );
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                      .AddEntityFrameworkStores<FactoryContext>()
+                      .AddDefaultTokenProviders();
 
-            WebApplication app = builder.Build();
+      WebApplication app = builder.Build();
 
-            app.UseDeveloperExceptionPage();
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+      app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
 
-            app.UseRouting();
+      app.UseRouting();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+      app.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
-        }
+      app.Run();
     }
+  }
 }
